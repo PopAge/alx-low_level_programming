b0f74d4985465c9d73c86c62556a666c5a18b975
PopAge <edwinadjanor@gmail.com>
2023-06-20 15:12:38 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * Return: 0 if  no error else return non zero
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n);
+	return (0);
+}
