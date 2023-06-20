a8294368d93dbfdec10352743d3781d330e73e0a
PopAge <edwinadjanor@gmail.com>
2023-06-20 16:19:36 -0700

    alphabet soup

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point for the program
+ *
+ * Return: 0 if no error else return non zero value
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == 'e'|| i == 'q')
+			continue;
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
