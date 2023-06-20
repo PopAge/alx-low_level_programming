4348ec56d3bbc333250aa7d5f986fdf3bd1cb25f
PopAge <edwinadjanor@gmail.com>
2023-06-20 09:05:31 -0700

    printing with printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * Return:0 if no errors else return non zero
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece
+			of art,\n");
+	return (0);
+}
