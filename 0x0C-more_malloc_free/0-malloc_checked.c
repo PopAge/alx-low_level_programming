69d6ba76dc5b7f70f245cece77bb2c86488aee52
PopAge <edwinadjanor@gmail.com>
2023-10-23 21:22:22 -0700

    malloc fix

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,16 +1,19 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
- * function that allocates memory using malloc.
- * @b: size to allocate 
+ * malloc_checked - allocates memory using malloc.
+ * @b: size to allocate
  * Return: Nothing.
  */
+
 void *malloc_checked(unsigned int b)
 {
 	void *mem = malloc(b);
+
 	if (mem == NULL)
 	{
 		exit(98);
 	}
-	return mem;
+	return (mem);
 }
