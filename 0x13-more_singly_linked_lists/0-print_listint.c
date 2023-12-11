5611d97dbd8936e4aa164cb919a8eead7df20f40
PopAge <edwinadjanor@gmail.com>
2023-12-11 12:49:23 -0800

    print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - print elements of listint_t
+ * @h:linked list
+ *
+ * Return: node
+ */
+
+size_t print_listint(const listint_t *h)
+
+{
+	size_t node = 0;
+
+		while (h)
+		{
+			printf("%d\n", h->n);
+			node++;
+			h = h->next;
+		}
+		return (node);
+}
