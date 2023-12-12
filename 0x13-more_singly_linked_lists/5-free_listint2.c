d648e807eba19a9290f00f102199f92464cb7022
PopAge <edwinadjanor@gmail.com>
2023-12-11 16:39:46 -0800

    free list updated

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,19 +1,22 @@
 #include "lists.h"
 
 /**
- * free_listint - linked list has to be freed.
- * @head: listint_list yet to be freed.
+ * free_listint2 - free the linked list
+ * @head: The listint to be freed
  */
 
-void free_listint(listint_t *head)
+void free_listint2(listint_t **head)
 
 {
 	listint_t *temp;
 
-	while (head)
+	if (head == NULL)
+	return;
+	while (*head)
 	{
-		temp = head->next;
-		free(head);
-		head = temp;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
 	}
+	*head = NULL;
 }
