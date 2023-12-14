d9928d7f131b5add61b2d901bf93bf25c7678c66
PopAge <edwinadjanor@gmail.com>
2023-12-14 13:44:04 -0800

    print binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,19 +9,21 @@ void print_binary(unsigned long int n)
 
 {
 	int x, count = 0;
-	unsigned long int crnt;
+	unsigned long int current;
 
 	for (x = 60; x >= 0; x--)
 	{
-		crnt = n >> x;
+		current = n >> x;
 
-		if (crnt & 1)
+		if (current & 1)
 		{
 			_putchar('1');
 			count++;
 		}
 		else if (count)
+		{
 			_putchar('0');
+		}
 	}
 	if (!count)
 		_putchar('0');
